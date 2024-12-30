887bdc31dab71765618ad071ddc665e40c62a59f
Jonathan Melitski <jonathanmelitski@gmail.com>
2024-12-30 11:41:27 -0500

    Initial Commit

---
+++ SpriteKitTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpriteKitTest
+//
+//  Created by Jonathan Melitski on 12/30/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
